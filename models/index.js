6a0fcf2db051954920fdc2244b21abfd94adcbcb
Brandon Akers <brandonakers729@gmail.com>
2021-07-29 18:25:35 -0500

    file structure

---
+++ models/index.js
@@ -0,0 +1,16 @@
+// // new mongoose schema
+// //day: { type: date, default: Now}
+// exercises: [
+//     {
+//         type: {
+//             type: String,
+//             trim: true
+//         }
+//         name: {}
+//         weight: {}
+//         sets: {}
+//         reps: {}
+//         duration: {}
+//         distance: {}
+//     }
+//
