1d1dfb8feffe7a06eca15ac001dcf1d62e4aca8b
Brandon Akers <brandonakers729@gmail.com>
2021-09-01 15:51:50 -0500

    updated server file

--- models/workout.js
+++ models/workout.js
@@ -1,16 +1,1 @@
-// // new mongoose schema
-// //day: { type: date, default: Now}
-// exercises: [
-//     {
-//         type: {
-//             type: String,
-//             trim: true
-//         }
-//         name: {}
-//         weight: {}
-//         sets: {}
-//         reps: {}
-//         duration: {}
-//         distance: {}
-//     }
-//
+
