a32df4a05b4e6ae8b9d8b331baf66bcd4eda3701
Brandon Akers <brandonakers729@gmail.com>
2021-09-01 21:30:58 -0500

    updating server

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect(
-  "mongodb+srv://bakers729:Brando1990$$@cluster0.oveoc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 app.listen(PORT, () => {
