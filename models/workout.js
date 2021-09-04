c3b3201d21d9ef5c779f7f1cc5abf39ec0b42a61
Brandon Akers <brandonakers729@gmail.com>
2021-09-04 12:24:47 -0500

    fixing workouts

--- models/workout.js
+++ models/workout.js
@@ -1,22 +1,47 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const workoutSchema = new Schema({
-  day: { type: Date, default: () => new Date() },
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
   exercises: [
     {
       type: {
         type: String,
         trim: true,
       },
-      name: {},
-      weight: {},
-      sets: {},
-      reps: {},
-      duration: {},
-      distance: {},
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+        default: 0,
+      },
+      reps: {
+        type: Number,
+        default: 0,
+      },
+      sets: {
+        type: Number,
+        default: 0,
+      },
+      distance: {
+        type: Number,
+        default: 0,
+      },
     },
   ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  },
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
 module.exports = Workout;
